67855c852f7ea0c1a2ed002e3e04bfb918f5bc04
qn0361 <qn0361@yandex.ru>
2020-07-18 18:35:43 +0300

    feat(strategies) мат. стратегия с персональными модификаторами

--- strategies.ts
+++ strategies.ts
@@ -1,6 +1,6 @@
 import {getWinChance} from './chances';
 import {sum} from './utils';
-import {keys, values} from 'lodash';
+import {keys, random, values} from 'lodash';
 
 const getMinBet = (bets: Bets, id: Id) => {
 	const maxBet = Math.max.apply(null, ...values(bets).map((bet) => bet || 0));
@@ -8,26 +8,45 @@ const getMinBet = (bets: Bets, id: Id) => {
 	return maxBet - (bets[id] || 0);
 };
 
-export const mathStrategy = (state: PublicGameState, id: Id): Move => {
-	const {acting, banks, bets, board, hands} = state;
-	const myHand = hands[id];
-	const against = keys(acting).length - 1;
-	const minBet = getMinBet(bets, id);
-	const needBet = minBet > 0;
-	const bank = sum(...values(banks));
-	const winBank = bank + minBet;
-	const winChance = getWinChance(myHand, against, board);
-	const bankChance = minBet / winBank;
-	const positive = winChance > bankChance;
+/**
+ *  @param {number} tightness [0, Infinity] (0 — totally loose, Infinity — never goes in, 0.8 — 1.2 values are normal)
+ * 	@param {number} sanity [0, 1] -- (0 — decisions are made totally on chances, 1 — on luck)
+ * */
+export const createMathStrategy = (tightness: number, sanity: number) => {
+	const getPersonalBankChance = (bankChance: number) => {
+		const coin = random(-sanity, sanity, true) * bankChance;
 
-	return needBet && positive ? {
-		value: minBet,
-		type: 'bet',
-	} : needBet && !positive ? {
-		type: 'fold',
-	} : {
-		type: 'bet',
-		value: winChance * (bank + bank * winChance),
+		return Math.floor(bankChance * tightness + coin);
+	};
+
+	const getPersonalRaiseValue = (winChance: number, bank: number) => {
+		const coin = random(-sanity, sanity, true) * winChance * bank;
+		const mathBasedBet = winChance * (bank + bank * winChance);
+
+		return Math.floor(mathBasedBet / tightness + coin);
+	};
+
+	return (state: PublicGameState, id: Id): Move => {
+		const {acting, banks, bets, board, hands} = state;
+		const myHand = hands[id];
+		const against = keys(acting).length - 1;
+		const minBet = getMinBet(bets, id);
+		const needBet = minBet > 0;
+		const bank = sum(...values(banks));
+		const winBank = bank + minBet;
+		const winChance = getWinChance(myHand, against, board);
+		const bankChance = minBet / winBank;
+		const positive = winChance > getPersonalBankChance(bankChance);
+
+		return needBet && positive ? {
+			value: minBet,
+			type: 'bet',
+		} : needBet && !positive ? {
+			type: 'fold',
+		} : {
+			type: 'bet',
+			value: getPersonalRaiseValue(winChance, bank),
+		};
 	};
 };
 
